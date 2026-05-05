faac070b40d95d173e70888ce9f4645f1adde082
obadaalshammat2009-sketch <obada.alshammat2009@gmail.com>
2026-05-05 10:24:15 +0200

    xyz

---
+++ Slutprojekt_GYM/html/menutoggle.js
@@ -0,0 +1,12 @@
+const toggleButton = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".menu");
+
+toggleButton.addEventListener("click", () => {
+  menu.classList.toggle("active");
+
+  const isOpen = menu.classList.contains("active");
+  toggleButton.setAttribute("aria-expanded", isOpen);
+});
+function fakeBuy() {
+  alert("Added to cart! (Demo only)");
+}
